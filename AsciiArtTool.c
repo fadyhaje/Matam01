c8074045ca0f30480ed92fd66f1ceda49864fb71
mariamassdi <117951268+mariamassdi@users.noreply.github.com>
2022-11-25 23:39:01 +0200

    Update AsciiArtTool.c

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -5,17 +5,17 @@
 RLEList asciiArtRead(FILE* in_stream)
 {
     //if(stream==NULL)
-       // return NULL;
+    // return NULL;
     char c;
-   RLEList list= RLEListCreate();
+    RLEList list= RLEListCreate();
     if(list==NULL){
         return NULL;
     }
-   //Set_Data(list, fgetc(stream));
-   //Set_times(list,1);
+    //Set_Data(list, fgetc(stream));
+    //Set_times(list,1);
     while(!feof(in_stream))
     {
-        c=fgetc(stream);
+        c=fgetc(in_stream);
         RLEListAppend(list,c);
     }
     return list;
@@ -32,34 +32,34 @@ RLEListResult asciiArtPrint(RLEList list, FILE* out_stream)
     RLEListResult result;
     int list_size=RLEListSize(list);
     while(i<list_size){
-    {
-        data=RLEListGet(list,i,&result);
-        if(resul!=RLE_LIST_SUCCESS)
         {
-            return result;
+            data=RLEListGet(list,i,&result);
+            if(result!=RLE_LIST_SUCCESS)
+            {
+                return result;
+            }
+            else
+            {
+                fprintf(out_stream,"%c",data);
+            }
+            i++;
         }
-        else
-        {
-            fprintf(out_stream,"%c",data);
-        }
-        i++;
-    }
-    return RLE_LIST_SUCCESS;
-}
+        return RLE_LIST_SUCCESS;
+    }}
 
- RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream)
-{
-    if(out_stream==NULL || list==NULL)
+    RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream)
     {
-        return RLE_LIST_NULL_ARGUMENT;
+        if(out_stream==NULL || list==NULL)
+        {
+            return RLE_LIST_NULL_ARGUMENT;
+        }
+        RLEListResult result;
+        char* list_string=RLEListExportToString(list, &result);
+        if(list_string==NULL)
+        {
+            return RLE_LIST_NULL_ARGUMENT;
+        }
+        fprintf(out_stream, "%s", list_string);
+        free(list_string);
+        return result;
     }
-    RLEListResult result;
-    char* list_string=RLEListExportToString(list, &result);
-    if(list_string==NULL)
-    {
-        return RLE_LIST_NULL_ARGUMENT;
-    }
-    fprintf(out_stream, "%s", list_string);
-    free(list_string);
-    return result;
-}
