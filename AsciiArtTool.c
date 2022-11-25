981be77c29eb01a36dc366aae231032079264732
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2022-11-25 16:33:19 +0200

    Update AsciiArtTool.c

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -7,14 +7,14 @@ RLEList asciiArtRead(FILE* in_stream)
         return NULL;
     char c;
    RLEList list= RLEListCreate();
+    if(list==NULL){
+        return NULL;
+    }
    Set_Data(list, fgetc(stream));
    Set_times(list,1);
-
-    while((c= fgetc(stream))!=EOF)
+    while((c==fgetc(stream))!=EOF)
     {
-
         RLEListAppend(list,c);
-
     }
     fclose(stream);
     return list;
