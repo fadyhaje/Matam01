243f79cc79754902972ed9ddaadf704001b6ded8
fadyhaje <103383097+fadyhaje@users.noreply.github.com>
2022-11-25 16:32:03 +0200

    Update AsciiArtTool.c

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -19,3 +19,45 @@ RLEList asciiArtRead(FILE* in_stream)
     fclose(stream);
     return list;
 }
+
+RLEListResult asciiArtPrint(RLEList list, FILE* out_stream)
+{
+    if(out_stream==NULL || list==NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    char data;
+    int i;
+    RLEListResult result;
+    while(i<RLEListSize(list)){
+    {
+        data=RLEListGet(list,i,&result);
+        if(resul!=RLE_LIST_SUCCESS)
+        {
+            return result;
+        }
+        else
+        {
+            fprintf(out_stream,"%c",data);
+        }
+        i++;
+    }
+    return RLE_LIST_SUCCESS;
+}
+
+ RLEListResult asciiArtPrintEncoded(RLEList list, FILE* out_stream)
+{
+    if(out_stream==NULL || list==NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEListResult result;
+    char* list_string=RLEListExportToString(list, &result);
+    if(list_string==NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    fprintf(out_stream, "%s", list_string);
+    free(list_string);
+    return result;
+}
